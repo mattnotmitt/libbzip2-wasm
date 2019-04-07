651f95df21d5831b7aad5476bfd0fbdca5c45587
Matt <matt@artemisbot.uk>
2019-04-07 18:22:04 +0100

    Added bzip2 decompression

--- main.cpp
+++ main.cpp
@@ -45,7 +45,43 @@ BZ2CC compressBZ2 (std::string input, int block_size, int work_factor) {
             resp.error_msg = "Unknown error.";
             break;
     }
-    std::cout << resp.error_msg << std::endl;
+    return resp;
+}
+
+BZ2CC decompressBZ2 (std::string input, int small) {
+    BZ2CC resp;
+    // Convert C++ strings to terrible C pointer arrays
+    auto inp = (char*) input.c_str();
+    resp.outLen = (unsigned int) 10000000; // I'm pretty sure that bzip2 isn't *this* good
+    char* out = new char[resp.outLen];
+    resp.error = BZ2_bzBuffToBuffDecompress(out, &resp.outLen, inp, (unsigned int) input.length(), small, 0);
+    switch (resp.error) {
+        case BZ_OK:
+            resp.output = (unsigned char*) out;
+            break;
+        case BZ_CONFIG_ERROR:
+        case BZ_PARAM_ERROR:
+            resp.error_msg = "There's been an issue with libbzip2-wasm. Please file an issue at https://github.com/artemisbot/libbzip2-wasm.";
+            break;
+        case BZ_OUTBUFF_FULL:
+            resp.error_msg = "The decompressed data exceeds the length of the destination buffer. The max size of a decrypted file is 100MB.";
+            break;
+        case BZ_MEM_ERROR:
+            resp.error_msg = "libbzip2-wasm has run out of memory.";
+            break;
+        case BZ_DATA_ERROR:
+            resp.error_msg = "The input data did not pass the integrity checks.";
+            break;
+        case BZ_DATA_ERROR_MAGIC:
+            resp.error_msg = "The input data does not begin with the correct magic bytes.";
+            break;
+        case BZ_UNEXPECTED_EOF:
+            resp.error_msg = "The input data ends unexpectedly.";
+            break;
+        default:
+            resp.error_msg = "Unknown error.";
+            break;
+    }
     return resp;
 }
 
@@ -58,4 +94,5 @@ EMSCRIPTEN_BINDINGS(my_module) {
             .property("output", &BZ2CC::getOutput);
 
     emscripten::function("compressBZ2", &compressBZ2);
+    emscripten::function("decompressBZ2", &decompressBZ2);
 }
